352fb1887d5a75056be3e22753613b163d38c65b
Nicolas Ansell <nra862@uregina.ca>
2021-10-18 20:19:52 -0600

    lab5

--- script.js
+++ script.js
@@ -1,55 +1,61 @@
-$(document).ready( () => { 
-    let addBtn = document.getElementById("add-btn");
-    let sortBtn = document.getElementById("sort-btn");
-    let inputField =  document.getElementById("input-text");
-    let list = document.getElementById("dyn-list");
-    let inputArray = [];
-    let counter = 0;
-    
-    function generateListElement()
-    {
-        let div = document.createElement("div");
-        div.setAttribute('class', 'input-group mx-auto mb-2 w-50 list-element');
-        div.setAttribute('id', `list-item-${counter}`);
-        
-        let input = document.createElement("input");
-        input.setAttribute('type', 'text');
-        input.setAttribute('value', inputField.value);
-        input.setAttribute('readonly', 'true');
-        input.setAttribute('class', 'form-control list-element');
-        input.setAttribute('style', 'box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2); margin-top:5px;');
-        div.appendChild(input);
-        list.appendChild(div);
-       
-        
-      
-        let sortObject = {
-            id : `list-item-${counter}`,
-            value : inputField.value
-        }   
-        inputArray.push(sortObject);
-        inputField.value = "";
-        counter++;
-        
-    }
-    function sortList(){
-        function compare( a, b ) {
-            if ( a.value < b.value ){
-              return -1;
-            }
-            if ( a.value > b.value ){
-              return 1;
-            }
-            return 0;
+
+    $(document).ready( () => { 
+        let addBtn = $("#add-btn")
+        let removeBtn = $("#remove-btn");
+        let inputField =  $("#input-text");
+        let list = $("#dyn-list");
+        let inputArray = [];
+        let counter = 0;
+        addBtn.click(generateListElement);
+        removeBtn.click(removeComplete);
+     
+        function generateListElement(){
+            list.after(`<div class='input-group mx-auto mb-2 w-50 py-2 mb-2' id='list-item-${counter}'><input type='text' class='form-control' value='${inputField.val() }' id="list-input-${counter}" readonly><button class='input-group-text' id='list-btn-${counter}'>Claim</button></div>`);
+            $(`#list-btn-${counter}`).click(claimTask);
+            let sortObject = {
+                id : `list-item-${counter}`,
+                value : inputField.value
+            }   
+            inputArray.push(sortObject);
+            inputField.val("");
+            counter++;
+            
         }
-        inputArray.sort(compare);//
-        console.log(inputArray); //
-        inputArray.forEach(input => {
-            list.appendChild(document.getElementById(input.id));
-        });
-    }
     
-    addBtn.addEventListener('click', generateListElement);
-    sortBtn.addEventListener('click', sortList);
-   });
-   
+        function claimTask(event){
+            let id = event.currentTarget.id.at(-1);
+            let btn = $(`#list-btn-${id}`);
+            let input = $(`#list-input-${id}`);
+            btn.html("Abandon");
+            input.parent().removeClass("input-group py-2");
+            input.parent().addClass("input-group-text px-0 py-0");
+            input.before(`<input class='form-check mx-2' type='checkbox' id='list-checkbox-${id}'>`)    
+            btn.click(abandonTask);
+            $(`#list-checkbox-${id}`).click(completeTask);
+            
+
+        }
+        function abandonTask(event){
+            let id = event.currentTarget.id.at(-1);
+            let btn = $(`#list-btn-${id}`);
+            let input = $(`#list-input-${id}`);
+            $("input").remove(`#list-checkbox-${id}`);
+            btn.html("Claim");
+            input.parent().addClass("input-group py-2");
+            input.parent().removeClass("input-group-text px-0 py-0");
+            btn.click(claimTask);
+
+        }
+        });
+
+        function completeTask(event){
+            let id = event.currentTarget.id.at(-1);
+            let btn = $(`#list-btn-${id}`);
+            let input = $(`#list-input-${id}`);
+            let checkbox = $(`#list-checkbox-${id}`);
+            btn.toggle();
+            input.toggleClass("text-decoration-line-through completed");
+        }
+        function removeComplete(){
+            $('.completed').parent().remove();
+        }
